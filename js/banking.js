926478b800c62e469040623d70144ee35d5f2426
AriyanShihab <devshihab111@gmail.com>
2022-08-16 21:20:14 +0600

    fixing NaN and do some styling

--- js/banking.js
+++ js/banking.js
@@ -1,87 +1,57 @@
 // deposit button handeling
 const dipositButton = document.getElementById("depositButton");
 dipositButton.addEventListener("click", function() {
+    // do reffarence
     const depositInput = document.getElementById("depositInput");
     const dipositAmount = document.getElementById("dipositAmount");
     const balanceAmount = document.getElementById("balanceAmount");
+    // input validation
+    if (depositInput.value === "") {
+        alert(
+            "vai tumi ki amake boka paiso? ami jhanker vai student! koto tk diposit korba bolo;"
+        );
+        return;
+    }
+    // conver to Number
     const userDeposit = parseFloat(depositInput.value);
     const totallDeposit = parseFloat(dipositAmount.innerText);
     let updateBalance = parseFloat(balanceAmount.innerText);
+
+    // basic calulation
+
     dipositAmount.innerText = totallDeposit + userDeposit;
     let blance = updateBalance + userDeposit;
     balanceAmount.innerText = blance;
     depositInput.value = "";
 });
 
-// handel withdray
+// handel withdraw
 
 const withdrawButton = document.getElementById("withdrawButton");
 
 withdrawButton.addEventListener("click", function() {
+    // getting dom reffarence
     const userWithdrawAmount = document.getElementById("withDrawInput");
     const balanceAmount = document.getElementById("balanceAmount");
     const totallWithdraw = document.getElementById("withdrawAmount");
+    if (userWithdrawAmount.value === "") {
+        alert(
+            "vai tumi ki amake boka paiso? ami jhanker vai student! koto tk withdraw korba bolo;"
+        );
+        return;
+    }
     // convert them in numbner;
     const withdrawInputAmount = parseFloat(userWithdrawAmount.value);
     const balanceInNumber = parseFloat(balanceAmount.innerText);
     const totallWithdrawInNumber = parseFloat(totallWithdraw.innerText);
     // calculation;
-    console.log(withdrawInputAmount, balanceInNumber, totallWithdrawInNumber);
+    if (balanceInNumber < withdrawInputAmount) {
+        alert("vai ki kortaso tumi? tomar babar bank a ato tk nai");
+        userWithdrawAmount.value = "";
+        return;
+    }
     balanceAmount.innerText = balanceInNumber - withdrawInputAmount;
     totallWithdraw.innerText = totallWithdrawInNumber + withdrawInputAmount;
 
     userWithdrawAmount.value = "";
 });
-
-/*
-// handle deposit button event
-document.getElementById('deposit-button').addEventListener('click', function () {
-    // get the amount deposited
-    const depositInput = document.getElementById('deposit-input');
-    const newDepositAmountText = depositInput.value;
-    const newDepositAmount = parseFloat(newDepositAmountText);
-
-    // update deposit total
-    const depositTotal = document.getElementById('deposit-total');
-    const previousDepositText = depositTotal.innerText;
-    const previousDepositAmount = parseFloat(previousDepositText);
-    const newDepositTotal = previousDepositAmount + newDepositAmount;
-
-    depositTotal.innerText = newDepositTotal;
-
-    // update account balance 
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
-    balanceTotal.innerText = newBalanceTotal;
-    // clear the deposit input field
-    depositInput.value = '';
-});
-
-// handle withdraw event handler
-document.getElementById('withdraw-button').addEventListener('click', function () {
-    const withdrawInput = document.getElementById('withdraw-input');
-    const withdrawAmountText = withdrawInput.value;
-    const newWithdrawAmount = parseFloat(withdrawAmountText);
-    console.log(newWithdrawAmount);
-
-    // set withdraw total
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const previousWithdrawText = withdrawTotal.innerText;
-    const previousWithdrawTotal = parseFloat(previousWithdrawText);
-
-    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotal.innerText = newWithdrawTotal;
-
-    // update balance
-    const balanceTotal = document.getElementById('balance-total');
-    const previousBalanceText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(previousBalanceText);
-    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-
-    balanceTotal.innerText = newBalanceTotal;
-
-    // clear withdraw input
-    withdrawInput.value = '';
-}) */
